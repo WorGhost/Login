81fc8eb25ecbd2102b2386477c53c0bc90a81464
WorGhost <57911305+WorGhost@users.noreply.github.com>
2024-05-30 16:00:00 -0400

    Create phase

--- BACK/src/controllers/phase.js
+++ BACK/src/controllers/phase.js
@@ -1,20 +1,28 @@
 import Phase from "../models/phases.js";
 import Project from "../models/project.js";
+import User from "../models/user.js";
 
 export const createPhase = async (req, res) => {
-  try {
+ try {
     const { name, date, user, hour } = req.body
     const id = req.params.idProject;
+    const users = req.user;
+    const userFind = await User.findById({ _id: users.id });
+    const isAdmin = userFind.isAdmin;
+    const project = await Project.findById({ _id: id });
+    
+     if (!isAdmin  || project.manager.toString() == users.id ) {
+       return res.status(401).send({ error: "No está autorizado para realizar esta acción" });
+     }
+    
     const phase = new Phase({ name, date, user, hour });
     const newPhase = await phase.save();
-    console.log(id)
 
      const updatedProject = await Project.findByIdAndUpdate(
        id,
         {$push: { phase: phase._id }},
         {new: true},
      );
-     console.log(updatedProject)
 
     res.status(201).send(newPhase);
   } catch (err) {
--- BACK/src/routes/phase.js
+++ BACK/src/routes/phase.js
@@ -1,9 +1,10 @@
 import Router from 'express'
 import { createPhase } from '../controllers/phase.js'
+import { authRequire } from '../middlewares/validateToken.js'
 
 const router = Router()
 
 router.route('/project/:idProject/phase')
-   .post(createPhase)
+   .post( authRequire,createPhase)
 
 export default router
